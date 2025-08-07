00ef06453e31029381c947c2ec170655f8192365
z0neSec <niklausdavid30@gmail.com>
2025-08-07 11:53:22 +0100

    mongo cloud

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use('/auth/x', twitterAuth);
 app.use('/api/vote', voteApi);
 app.use('/user_memes', express.static(path.join(__dirname, '../public/user_memes')));
 
-mongoose.connect('mongodb://localhost:27017/memeit', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
